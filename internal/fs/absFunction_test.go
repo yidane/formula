0106dc47fb14e2bece9228c848a9ba27bf00eeeb
yidane <yidane@163.com>
2019-04-28 17:53:51 +0800

    add absFunction_test.go: THE LAST COMMIT FROM NXIN. 20190428175349

--- internal/fs/absFunction_test.go
+++ internal/fs/absFunction_test.go
@@ -1,10 +1,42 @@
 package fs
 
-import "testing"
+import (
+	"fmt"
+	"github.com/yidane/formula/internal/exp"
+	"testing"
 
-func TestNewAbsFunction(t *testing.T){
-	abs:= NewAbsFunction()
-	if abs.Name()!="Abs"{
-		t.Fatal()
+	"github.com/yidane/formula/opt"
+)
+
+func TestAbsFunction_Evaluate(t *testing.T) {
+	tests := []struct {
+		args []string
+		want []float64
+	}{
+		{[]string{"1", "2", "1.1", "-1", "2", "-3"}, []float64{1, 2, 1.1, 1, 2, 3}},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.args), func(t *testing.T) {
+			f := &AbsFunction{}
+
+			for i := 0; i < len(tt.args); i++ {
+				var logicExpression = *exp.NewIntegerValueExpression(tt.args[i])
+
+				result, err := f.Evaluate(nil, []*opt.LogicalExpression{&logicExpression}...)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				v, err := result.Float64()
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if v != tt.want[i] {
+
+				}
+			}
+
+		})
 	}
 }
